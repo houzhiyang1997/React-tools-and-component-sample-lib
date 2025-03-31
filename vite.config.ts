222328fcc5df783d5d0899df1532750ddc7bbdff
houzy <houzhiyang1997@163.com>
2025-03-31 16:32:33 +0800

    chore: 添加.env文件并配置vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from "vite";
+import { readFileSync } from "fs";
 import react from "@vitejs/plugin-react-swc";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+import { parse } from "dotenv";
 
-// https://vite.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const env = parse(readFileSync(resolve(__dirname, ".env")));
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: resolve(__dirname, "./src") },
+      { find: "@common", replacement: resolve(__dirname, "./common") },
+      { find: /^~/, replacement: "" },
+    ],
+  },
+  server: {
+    host: "0.0.0.0",
+    https: {},
+    cors: true,
+    proxy: {
+      "^/(api|auth|header)/": {
+        target: env.VITE_GATEWAY_URL,
+        changeOrigin: true,
+        autoRewrite: true,
+        secure: false,
+      },
+    },
+  },
 });
