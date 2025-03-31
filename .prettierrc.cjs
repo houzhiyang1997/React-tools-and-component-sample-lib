c3dd5321b7b977598790dc6e0303e24d1369538d
houzy <houzhiyang1997@163.com>
2025-03-31 13:32:00 +0800

    style: 统一eslint与prettier规则
    此次提交主要进行了代码格式的统一调整，包括缩进、引号使用等，并简化了.prettierrc.cjs的配置，以提升代码的可读性和一致性。

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,26 +1,5 @@
+const prettierConfig = require("eslint-config-prettier");
+
 module.exports = {
-    printWidth: 120,
-    tabWidth: 4,
-    useTabs: false,
-    semi: true,
-    singleQuote: true,
-    quoteProps: 'as-needed',
-    jsxSingleQuote: false,
-    trailingComma: 'all',
-    bracketSpacing: true,
-    bracketSameLine: false,
-    arrowParens: 'always',
-    proseWrap: 'preserve',
-    htmlWhitespaceSensitivity: 'css',
-    endOfLine: 'lf',
-    embeddedLanguageFormatting: 'off',
-    singleAttributePerLine: false,
-    overrides: [
-        {
-            files: ['*.json'],
-            options: {
-                tabWidth: 2,
-            },
-        },
-    ],
+    ...prettierConfig
 };
